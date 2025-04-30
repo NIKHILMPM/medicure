bbdc3531bd7955481ddaf2d0efac3ceacb102878
NIKHILMPM <157490189+NIKHILMPM@users.noreply.github.com>
2025-04-30 12:18:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "medicure_ec2" {
-  ami                         = "ami-0f9de6e2d2f067fca"
+  ami                         = "ami-053b0d53c279acc90"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   key_name                    = "jjk"
